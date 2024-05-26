c0de53cd9d7919c39de60eccfdc0d4e4910c6083
Xuan_Hai <xuanhai2708@gmail.com>
2024-05-26 18:20:10 +0300

    init: base project

---
+++ src/components/AddStudentForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddStudentForm = () => {
+  return <div></div>;
+};
+
+export default AddStudentForm;
---
+++ src/components/DeleteStudentButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeleteStudentButton = () => {
+  return <div></div>;
+};
+
+export default DeleteStudentButton;
