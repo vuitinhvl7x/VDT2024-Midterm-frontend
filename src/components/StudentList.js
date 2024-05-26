e12ce328684abe1fd250579d5394ce3586d696a6
Xuan_Hai <xuanhai2708@gmail.com>
2024-05-26 18:22:29 +0300

    feat: add StudentList

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import StudentItem from "./StudentItem";
+import AddStudentForm from "./AddStudentForm";
 
-const StudentList = () => {
-  return <div></div>;
-};
+function StudentList() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  const fetchStudents = async () => {
+    try {
+      const response = await axios.get("http://localhost:4000/api/students");
+      setStudents(response.data);
+    } catch (error) {
+      console.error("Error fetching students:", error);
+    }
+  };
+
+  return (
+    <div className="container mx-auto p-4">
+      <AddStudentForm setStudents={setStudents} />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+        {students.map((student) => (
+          <StudentItem
+            key={student.id}
+            student={student}
+            setStudents={setStudents}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default StudentList;
