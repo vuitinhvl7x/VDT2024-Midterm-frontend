8b78b832d05adcd524e29b6a4afb9a28ee1a40ed
Xuan_Hai <xuanhai2708@gmail.com>
2024-06-13 03:15:38 +0300

    feat: apiurl

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -5,7 +5,7 @@ const sendToAnalytics = (metric) => {
   const body = JSON.stringify(metric);
 
   axios
-    .get("http://10.103.152.171:4000/api/metrics", body, {
+    .get("http://192.168.56.61:30001/api/metrics", body, {
       headers: {
         "Content-Type": "application/json",
       },
