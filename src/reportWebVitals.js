167f4cb7fde1023b83ddef46bb953c55eacb3643
Xuan_Hai <xuanhai2708@gmail.com>
2024-06-13 05:09:44 +0300

    fix: fix apiurl metrics

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -5,7 +5,7 @@ const sendToAnalytics = (metric) => {
   const body = JSON.stringify(metric);
 
   axios
-    .get("http://192.168.56.61:30001/api/metrics", body, {
+    .get("http://192.168.56.61:30001/metrics", body, {
       headers: {
         "Content-Type": "application/json",
       },
